44dee27e94c005964eac2d0a646d721dd9a9d325
Artem <lebediantsevartem@gmail.com>
2021-06-18 08:01:45 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu'),
+    closeElem = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+closeElem.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
+//значок вверх страницы, скролл
+$(window).scroll(function() {
+    if($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+
+  /* $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+    }).done(function() {
+      $(this).find("input").val("");
+
+      $('form').trigger('reset');
+    });
+    return false;
+  }); */
+
+
+
+
+
